4c864466f71b8178c49c95f8340edf8ed007f665
rtyty0615 <174629541+rtyty0615@users.noreply.github.com>
2026-01-16 17:13:06 +0800

    Added a button with popup asking for the number of squares per side for the new grid.

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const container = document.getElementById("container");
 
-let i = 0;
-for (let i = 1; i <= 16; i++){
-    const div_row = document.createElement("div");
-    div_row.id = "square_row_" + i;
-    container.appendChild(div_row);
-    const square_row = document.getElementById("square_row_" + i);
-    for (let j = 1; j <= 16; j++) {
-        const div_column = document.createElement("div");
-        div_column.id = "square_column_" + j;
-        div_column.addEventListener("mouseover", () => {
-            div_column.style.backgroundColor = "red";
-        });
-        square_row.appendChild(div_column);
-    }
-}
 
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    let square_number = prompt("Enter new canvas size (1-100, e.g: 16)", "16");
+    if (!/^[0-9.,]+$/.test(square_number)){
+        alert("Enter a whole number (1-100)")};
+    const num = parseInt(square_number, 10);
+    if (num < 1 || num > 100) {
+        alert("Must be 1-100");
+    } else {
+        if (container.firstElementChild){
+            container.innerHTML = "";
+        }
+        let i = 0;
+        for (let i = 1; i <= num; i++) {
+            const div_row = document.createElement("div");
+            div_row.id = "square_row_" + i;
+            container.appendChild(div_row);
+            const square_row = document.getElementById("square_row_" + i);
+            for (let j = 1; j <= num; j++) {
+                const div_column = document.createElement("div");
+                div_column.id = "square_column_" + j;
+                div_column.addEventListener("mouseover", () => {
+                    div_column.style.backgroundColor = "red";
+                });
+                square_row.appendChild(div_column);
+            }
+        }
+    }
+
+})
